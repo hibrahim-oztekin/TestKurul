8f346511e64bb7af7a25815669d670d9eb1fd7fa
hibrahim-oztekin <71639544+hibrahim-oztekin@users.noreply.github.com>
2024-08-30 21:32:30 +0300

    tawild

---
+++ MauiApp/MauiAppHukuk/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppHukuk;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MauiApp/MauiAppHukuk/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppHukuk;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
